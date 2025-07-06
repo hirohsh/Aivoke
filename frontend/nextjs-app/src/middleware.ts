39684dbde80947ca68c1c3f9b429d6626f7e0c85
hhiro <waon.jpkbk@gmail.com>
2025-07-06 09:11:05 +0900

    リダイレクト時にレスポンスをそのまま返すように変更

--- frontend/nextjs-app/src/middleware.ts
+++ frontend/nextjs-app/src/middleware.ts
@@ -4,12 +4,7 @@ export async function middleware(request: NextRequest) {
   const isDev = process.env.NODE_ENV !== 'production';
   // nonce を生成
   const bytes = crypto.getRandomValues(new Uint8Array(16)); // Web Crypto
-  const nonce = btoa(String.fromCharCode(...bytes));
-
-  const reqHeaders = new Headers(request.headers);
-  reqHeaders.set('x-nonce', nonce);
-
-  const response = (await updateSession(request)) || NextResponse.next();
+  const nonce = Buffer.from(bytes).toString('base64url');
 
   const csp = [
     "default-src 'self'",
@@ -23,19 +18,26 @@ export async function middleware(request: NextRequest) {
     "base-uri 'self'",
     "frame-ancestors 'none'",
     `form-action 'self'`,
-    `connect-src 'self' https://*.supabase.co http://host.docker.internal:54321`,
+    `connect-src 'self' https://*.supabase.co ${isDev ? 'http://host.docker.internal:54321' : ''}`,
   ].join('; ');
 
   const contentSecurityPolicyHeaderValue = csp.replace(/\s{2,}/g, ' ').trim();
 
-  reqHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+  const supaRes = await updateSession(request);
+
+  if (supaRes?.headers.has('location')) {
+    supaRes.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+    return supaRes;
+  }
+
+  const reqHeaders = new Headers(request.headers);
+  reqHeaders.set('x-nonce', nonce);
 
   const res = NextResponse.next({ request: { headers: reqHeaders } });
-  response.cookies.getAll().forEach((cookie) => {
-    res.cookies.set(cookie.name, cookie.value, { path: cookie.path });
-  });
+
+  supaRes.cookies.getAll().forEach((c) => res.cookies.set(c.name, c.value, c));
+
   res.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
-  res.headers.set('x-nonce', nonce);
 
   return res;
 }
@@ -49,7 +51,7 @@ export const config = {
      * Feel free to modify this pattern to include more paths.
      */
     {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      source: '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
       missing: [
         { type: 'header', key: 'next-router-prefetch' },
         { type: 'header', key: 'purpose', value: 'prefetch' },
